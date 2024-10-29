7506ec6b903569fd0a43203505e4863b7db7d54c
Natanas404 <181837355+Natanas404@users.noreply.github.com>
2024-10-29 16:59:54 +0100

    fix: set "rock paper and scissors" to lowercase
    In getComputerChoice: Changes the return messages to lowercase
    In getHumanChoice: Changes the prompt's message to lowercase
    i.e. "rock" instead of "Rock" in the whole file

--- scriptRock_Paper_Scissors.js
+++ scriptRock_Paper_Scissors.js
@@ -2,11 +2,11 @@ function getComputerChoice () {
     let value = Math.random();
     console.log(value);
     if (value >= 0 && value <= 0.3) { // Rock will come out more often because it includes results >0 and <=0.1
-        return "Rock";
+        return "rock";
     } else if (value >= 0.4 && value <= 0.6) {
-        return "Paper";
+        return "paper";
     } else if (value >= 0.7 && value < 1) {
-        return "Scissors";
+        return "scissors";
     } else {
         getComputerChoice(); // to use once I know how to run the first if between 0.1 and 0.3 instead of 0 and 0.3
     }
@@ -14,7 +14,7 @@ function getComputerChoice () {
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let humanChoice = window.prompt("Rock, Paper or Scissors?");
+    let humanChoice = window.prompt("rock, paper or scissors?");
     console.log(humanChoice);
 }
 console.log (getHumanChoice());
