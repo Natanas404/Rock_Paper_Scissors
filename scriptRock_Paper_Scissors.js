cfa2466fde6c7c9089b0e1249177b65b4f4b3e53
Natanas404 <181837355+Natanas404@users.noreply.github.com>
2024-10-29 15:38:44 +0100

    feat: create function to return R, P or S randomly
    Creates the function getComputerChoice which returns a random choice between Rock, Paper and Scissors.

--- scriptRock_Paper_Scissors.js
+++ scriptRock_Paper_Scissors.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function getComputerChoice () {
+    let value = Math.random();
+    console.log(value);
+    if (value >= 0 && value <= 0.3) { // Rock will come out more often because it includes results >0 and <=0.1
+        return "Rock";
+    } else if (value >= 0.4 && value <= 0.6) {
+        return "Paper";
+    } else if (value >= 0.7 && value < 1) {
+        return "Scissors";
+    } else {
+        getComputerChoice(); // to use once I know how to run the first if between 0.1 and 0.3 instead of 0 and 0.3
+    }
+}
+console.log(getComputerChoice());
