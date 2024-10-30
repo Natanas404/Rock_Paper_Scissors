9667809d03bed6d2eaff8202009c13c7329296bd
Natanas404 <181837355+Natanas404@users.noreply.github.com>
2024-10-30 15:35:46 +0100

    feat: create a while loop to play the game 5x
    Creates a while loop with the condition to play the game until the total score = 5
    It may end up being more than 5 rounds if there is a tie and/or if the player misspells their answer
    Moves all the functions and constances inside the playGame function to re-execute them all when starting a new round
    Keeps the score variables outside the playGame function for them to be accessed by while's condition

--- scriptRock_Paper_Scissors.js
+++ scriptRock_Paper_Scissors.js
@@ -1,29 +1,26 @@
-function getComputerChoice() {
-    let value = Math.random();
-    console.log(value);
-    if (value >= 0 && value <= 0.3) { // Rock will come out more often because it includes results >0 and <=0.1
-        return "rock";
-    } else if (value >= 0.3 && value <= 0.6) { // used 0.3 again (and 0.6 again on the next line) to take into account the numbers between 0.3 and 0.4 (and 0.6 and 0.7)
-        return "paper";
-    } else if (value >= 0.6 && value < 1) {
-        return "scissors";
-    } else {
-        getComputerChoice(); //In case i've missed any other values.
-    }
-}
-
-function getHumanChoice() {
-    answer = window.prompt("rock, paper or scissors?");
-    return answer.toLowerCase();
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-
 function playGame() {
+    function getComputerChoice() {
+        let value = Math.random();
+        console.log(value);
+        if (value >= 0 && value <= 0.3) { // Rock will come out more often because it includes results >0 and <=0.1
+            return "rock";
+        } else if (value >= 0.3 && value <= 0.6) { // used 0.3 again (and 0.6 again on the next line) to take into account the numbers between 0.3 and 0.4 (and 0.6 and 0.7)
+            return "paper";
+        } else if (value >= 0.6 && value < 1) {
+            return "scissors";
+        } else {
+            getComputerChoice(); //In case i've missed any other values.
+        }
+    }
+
+    function getHumanChoice() {
+        answer = window.prompt("rock, paper or scissors?");
+        return answer.toLowerCase();
+    }
+
+    const computerSelection = getComputerChoice();
+    const humanSelection = getHumanChoice();
+
     function playRound(computerChoice, humanChoice) {
         if (humanChoice === computerChoice) {
             return `How unlucky, it's a tie! Try again. The score is: you = ${humanScore}; computer = ${computerScore}`;
@@ -42,4 +39,12 @@ function playGame() {
     console.log(playRound(computerSelection, humanSelection));
 }
 
-console.log(playGame())
+let humanScore = 0;
+let computerScore = 0;
+
+do {
+playGame();
+}
+while ((humanScore + computerScore) < 5); 
+
+console.log(`The game is over! Here is the final score: you ${humanScore}; computer ${computerScore}`);
