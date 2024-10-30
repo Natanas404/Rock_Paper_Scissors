c7c2c36758935c6f392f13a44a5a968ffdd54df0
Natanas404 <181837355+Natanas404@users.noreply.github.com>
2024-10-30 13:50:21 +0100

    fix: fixed values & deleted spaces in 1st function
    Changed the values of getComputerChoice to encompass more data
    Deleted additional spaces before parentheses
    Deleted the console.logs following the functions to avoid them from being executed more than necessary

--- scriptRock_Paper_Scissors.js
+++ scriptRock_Paper_Scissors.js
@@ -1,24 +1,46 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let value = Math.random();
     console.log(value);
     if (value >= 0 && value <= 0.3) { // Rock will come out more often because it includes results >0 and <=0.1
         return "rock";
-    } else if (value >= 0.4 && value <= 0.6) {
+    } else if (value >= 0.3 && value <= 0.6) { // used 0.3 again (and 0.6 again on the next line) to take into account the numbers between 0.3 and 0.4 (and 0.6 and 0.7)
         return "paper";
-    } else if (value >= 0.7 && value < 1) {
+    } else if (value >= 0.6 && value < 1) {
         return "scissors";
     } else {
-        getComputerChoice(); // to use once I know how to run the first if between 0.1 and 0.3 instead of 0 and 0.3
+        getComputerChoice(); //In case i've missed any other values.
     }
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let humanChoice = window.prompt("rock, paper or scissors?");
-    console.log(humanChoice);
+    answer = window.prompt("rock, paper or scissors?");
+    return answer.toLowerCase();
 }
-console.log (getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
 console.log(humanScore, computerScore);
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+function playRound(computerChoice, humanChoice) {
+    if (humanChoice === computerChoice) {
+        return "How unlucky, it's a tie! Try again.";
+    } else if (humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "rock") {
+        return "You've won! Try again if you dare.";
+    } else if (computerChoice === "rock" && humanChoice === "scissors"
+        || computerChoice === "scissors" && humanChoice === "paper"
+        || computerChoice === "paper" && humanChoice === "rock") {
+        return "You've lost. Try again to get your revenge!";
+    } else {
+        return "You might have spelt that wrong. Try again, please.";
+    }
+}
+
+console.log(playRound(computerSelection, humanSelection));
+
+
+//For some reason, you keep having "it's a tie" returned even if getHumanChoice and getComputerCHoice return different values.
